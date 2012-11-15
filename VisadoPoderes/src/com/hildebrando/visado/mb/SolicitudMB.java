ad5355b5fd5ed7221558b13d684f69dd18f6c7a4
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2012-11-15 22:04:30 +0000

    [15-11-2012]: Visado de Poderes: Pantalla Bandeja de Seguimiento.

--- VisadoPoderes/src/com/hildebrando/visado/mb/SolicitudMB.java
+++ VisadoPoderes/src/com/hildebrando/visado/mb/SolicitudMB.java
@@ -1,5 +1,8 @@
 package com.hildebrando.visado.mb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -10,9 +13,22 @@ import com.hildebrando.visado.dto.Solicitud;
 public class SolicitudMB {
 
 	Solicitud solicitudModificar;
-
+	Solicitud bandeja;
+	private List<Solicitud> solicitudes;
+	private String textoTotalResultados;
+	
 	public SolicitudMB() {
 		solicitudModificar = new Solicitud();
+		solicitudes= new ArrayList<Solicitud>();
+		
+		if (solicitudes.size()==0)
+		{
+			textoTotalResultados="No se encontraron coincidencias con los criterios ingresados";
+		}
+		else
+		{
+			textoTotalResultados="Total de registros: "+ solicitudes.size() + " registros";
+		}
 	}
 
 	public Solicitud getSolicitudModificar() {
@@ -23,6 +39,29 @@ public class SolicitudMB {
 		this.solicitudModificar = solicitudModificar;
 	}
 
-	
+	public Solicitud getBandeja() {
+		return bandeja;
+	}
 
+	public void setBandeja(Solicitud bandeja) {
+		this.bandeja = bandeja;
+	}
+
+	public List<Solicitud> getSolicitudes() {
+		return solicitudes;
+	}
+
+	public void setSolicitudes(List<Solicitud> solicitudes) {
+		this.solicitudes = solicitudes;
+	}
+
+	public String getTextoTotalResultados() {
+		return textoTotalResultados;
+	}
+
+	public void setTextoTotalResultados(String textoTotalResultados) {
+		this.textoTotalResultados = textoTotalResultados;
+	}
+
+	
 }
