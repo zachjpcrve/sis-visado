ac1ba3d9708f9d901c6f53c59bf29d9e83b14214
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2012-11-19 23:34:17 +0000

    [19-11-2012]: Visado de Poderes: Actualizacion fuentes Bandeja de Seguimiento

--- trunk/VisadoPoderes/src/com/hildebrando/visado/mb/SolicitudMB.java
+++ trunk/VisadoPoderes/src/com/hildebrando/visado/mb/SolicitudMB.java
@@ -1,10 +1,13 @@
 package com.hildebrando.visado.mb;
 
+import java.awt.event.ActionEvent;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
 
 import org.apache.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
@@ -14,14 +17,17 @@ import com.bbva.persistencia.generica.dao.Busqueda;
 import com.bbva.persistencia.generica.dao.GenericDao;
 import com.hildebrando.visado.dto.Estado;
 import com.hildebrando.visado.dto.EstadosNivel;
+import com.hildebrando.visado.dto.Moneda;
 import com.hildebrando.visado.dto.RangosImporte;
 import com.hildebrando.visado.dto.Solicitud;
 import com.hildebrando.visado.dto.TiposFecha;
 import com.hildebrando.visado.modelo.TiivsEstudio;
+import com.hildebrando.visado.modelo.TiivsMiembro;
 import com.hildebrando.visado.modelo.TiivsMultitabla;
 import com.hildebrando.visado.modelo.TiivsNivel;
 import com.hildebrando.visado.modelo.TiivsOficina1;
 import com.hildebrando.visado.modelo.TiivsOperacionBancaria;
+import com.hildebrando.visado.modelo.TiivsSolicitud;
 import com.hildebrando.visado.modelo.TiivsTerritorio;
 import com.hildebrando.visado.modelo.TiivsTipoDocumento;
 import com.hildebrando.visado.modelo.TiivsTipoServicio;
@@ -32,7 +38,7 @@ public class SolicitudMB
 {
 	Solicitud solicitudModificar;
 	Solicitud bandeja;
-	private List<Solicitud> solicitudes;
+	private List<TiivsSolicitud> solicitudes;
 	private String textoTotalResultados;
 	private TiivsMultitabla multitabla;
 	private List<TiivsMultitabla> lstMultitabla;
@@ -46,6 +52,9 @@ public class SolicitudMB
 	private List<TiivsTipoServicio> lstTipoSolicitud;
 	private List<TiivsTerritorio> lstTerritorio;
 	private List<TiivsOficina1> lstOficina;
+	private List<TiivsOficina1> lstOficina1;
+	private List<Moneda> lstMoneda;
+	private List<TiivsMiembro> lstMiembros;
 	private String idCodOfi;
 	private String idCodOfi1;
 	private String idTerr;
@@ -57,13 +66,15 @@ public class SolicitudMB
 	private String idTiposFecha;
 	private String idNivel;
 	private String idEstNivel;
+	private String poderdante;
+	private String codSolicitud;
 	
 	public static Logger logger = Logger.getLogger(SolicitudMB.class);
 	
 	public SolicitudMB() 
 	{
 		solicitudModificar = new Solicitud();
-		solicitudes= new ArrayList<Solicitud>();
+		solicitudes= new ArrayList<TiivsSolicitud>();
 		lstMultitabla=new ArrayList<TiivsMultitabla>();
 		lstRangosImporte=new ArrayList<RangosImporte>();
 		lstEstado = new ArrayList<Estado>();
@@ -75,6 +86,22 @@ public class SolicitudMB
 		lstTipoSolicitud=new ArrayList<TiivsTipoServicio>();
 		lstTerritorio=new ArrayList<TiivsTerritorio>();
 		lstOficina=new ArrayList<TiivsOficina1>();
+		lstOficina1=new ArrayList<TiivsOficina1>();
+		lstMoneda = new ArrayList<Moneda>();
+		cargarMultitabla();		
+		//Carga combo Rango Importes
+		cargarCombosBandejaSeguimiento("T07");
+		//Carga combo Estados
+		cargarCombosBandejaSeguimiento("T02");
+		//Carga combo Estados Nivel
+		cargarCombosBandejaSeguimiento("T09");
+		//Carga combo Tipos de Fecha
+		cargarCombosBandejaSeguimiento("T10");
+		//Carga lista de monedas
+		cargarCombosBandejaSeguimiento("T08");
+		cargarMiembros();
+		//LLena la grilla de solicitudes
+		cargarSolicitudes();
 		
 		if (solicitudes.size()==0)
 		{
@@ -84,16 +111,109 @@ public class SolicitudMB
 		{
 			textoTotalResultados="Total de registros: "+ solicitudes.size() + " registros";
 		}
+	}
+	
+	public void buscarSolicitudes(ActionEvent e)
+	{
+		GenericDao<TiivsSolicitud, Object> solicDAO = (GenericDao<TiivsSolicitud, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
+		Busqueda filtroSol= Busqueda.forClass(TiivsSolicitud.class);
 		
-		cargarMultitabla();
-		//Carga combo Rango Importes
-		cargarCombosBandejaSeguimiento("T07");
-		//Carga combo Estados
-		cargarCombosBandejaSeguimiento("T02");
-		//Carga combo Estados Nivel
-		cargarCombosBandejaSeguimiento("T09");
-		//Carga combo Tipos de Fecha
-		cargarCombosBandejaSeguimiento("T10");
+		if (getCodSolicitud().compareTo("")==0)
+		{
+			filtroSol.add(Restrictions.eq("codSoli", getCodSolicitud()));
+		}
+		
+		if (getIdCodOfi().compareTo("")==0)
+		{
+			filtroSol.createAlias("tiivsOficina1", "ofic");
+			filtroSol.add(Restrictions.eq("ofic.codOfi", getIdCodOfi()));
+		}
+		
+		if (getIdCodOfi1().compareTo("")!=0)
+		{
+			filtroSol.createAlias("tiivsOficina1", "ofic");
+			filtroSol.add(Restrictions.eq("ofic.codOfi", getIdCodOfi1()));
+		}
+		
+		if (getIdOpeBan().compareTo("")==0)
+		{
+			filtroSol.add(Restrictions.eq("operacionesBancarias", getIdOpeBan()));
+		}
+		
+		if (getIdImporte().compareTo("")==0)
+		{
+			if (getIdImporte().equals("0001"))
+			{
+				filtroSol.add(Restrictions.le("importe", 50));
+			}
+			
+			if (getIdImporte().equals("0002"))
+			{
+				filtroSol.add(Restrictions.gt("importe", 50));
+				filtroSol.add(Restrictions.le("importe", 120));
+			}
+			
+			if (getIdImporte().equals("0003"))
+			{
+				filtroSol.add(Restrictions.gt("importe", 120));
+				filtroSol.add(Restrictions.le("importe", 250));
+			}
+			
+			if (getIdImporte().equals("0004"))
+			{
+				filtroSol.add(Restrictions.gt("importe", 250));
+			}
+		}
+		
+		try {
+			solicitudes =  solicDAO.buscarDinamico(filtroSol);
+		} catch (Exception ex) {
+			logger.debug("Error al buscar las solicitudes");
+		}
+	}
+	
+	public void cargarSolicitudes()
+	{
+		GenericDao<TiivsSolicitud, Object> solicDAO = (GenericDao<TiivsSolicitud, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
+		Busqueda filtroSol= Busqueda.forClass(TiivsSolicitud.class);
+		
+		try {
+			solicitudes =  solicDAO.buscarDinamico(filtroSol);
+			System.out.println("Tamanio inicial de solicitudes: " + solicitudes.size());
+		} catch (Exception ex) {
+			logger.debug("Error al buscar las solicitudes");
+		}
+		
+		for (TiivsSolicitud tmpSol: solicitudes)
+		{
+			if (tmpSol.getMoneda()!=null)
+			{
+				if (tmpSol.getMoneda().equalsIgnoreCase(lstMoneda.get(0).getCodMoneda()))
+				{
+					tmpSol.setDesMoneda(lstMoneda.get(0).getDesMoneda());
+				}
+			}
+			if (tmpSol.getRegAbogado()!=null)
+			{
+				if (tmpSol.getRegAbogado().equals(lstMiembros.get(0).getCodMiembro()))
+				{
+					tmpSol.setEstudio(lstMiembros.get(0).getEstudio());
+				}
+			}
+		}
+	}
+	
+	public void cargarMiembros()
+	{
+		GenericDao<TiivsMiembro, Object> miemDAO = (GenericDao<TiivsMiembro, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
+		Busqueda filtroMiem= Busqueda.forClass(TiivsMiembro.class);
+		
+		try {
+			lstMiembros =  miemDAO.buscarDinamico(filtroMiem);
+			
+		} catch (Exception ex) {
+			logger.debug("Error al buscar registros de la tabla miembro");
+		}
 	}
 
 	public Solicitud getSolicitudModificar() 
@@ -116,12 +236,12 @@ public class SolicitudMB
 		this.bandeja = bandeja;
 	}
 
-	public List<Solicitud> getSolicitudes() 
+	public List<TiivsSolicitud> getSolicitudes() 
 	{
 		return solicitudes;
 	}
 
-	public void setSolicitudes(List<Solicitud> solicitudes) 
+	public void setSolicitudes(List<TiivsSolicitud> solicitudes) 
 	{
 		this.solicitudes = solicitudes;
 	}
@@ -320,6 +440,46 @@ public class SolicitudMB
 		this.idEstNivel = idEstNivel;
 	}
 
+	public List<TiivsOficina1> getLstOficina1() {
+		return lstOficina1;
+	}
+
+	public void setLstOficina1(List<TiivsOficina1> lstOficina1) {
+		this.lstOficina1 = lstOficina1;
+	}
+
+	public String getPoderdante() {
+		return poderdante;
+	}
+
+	public void setPoderdante(String poderdante) {
+		this.poderdante = poderdante;
+	}
+
+	public String getCodSolicitud() {
+		return codSolicitud;
+	}
+
+	public void setCodSolicitud(String codSolicitud) {
+		this.codSolicitud = codSolicitud;
+	}
+
+	public List<Moneda> getLstMoneda() {
+		return lstMoneda;
+	}
+
+	public void setLstMoneda(List<Moneda> lstMoneda) {
+		this.lstMoneda = lstMoneda;
+	}
+
+	public List<TiivsMiembro> getLstMiembros() {
+		return lstMiembros;
+	}
+
+	public void setLstMiembros(List<TiivsMiembro> lstMiembros) {
+		this.lstMiembros = lstMiembros;
+	}
+
 	public void cargarMultitabla()
 	{
 		GenericDao<TiivsMultitabla, Object> multiDAO = (GenericDao<TiivsMultitabla, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
@@ -330,6 +490,29 @@ public class SolicitudMB
 		} catch (Exception e) {
 			logger.debug("Error al cargar el listado de multitablas");
 		}
+	}
+	
+	public TiivsOficina1 buscarOficinaPorCodigo(ValueChangeEvent e) {
+		logger.debug("Buscando oficina por codigo: " + e.getNewValue());
+		GenericDao<TiivsOficina1, Object> ofiDAO = (GenericDao<TiivsOficina1, Object>) SpringInit.getApplicationContext().getBean("genericoDao");
+		Busqueda filtroOfic = Busqueda.forClass(TiivsOficina1.class);
+		filtroOfic.add(Restrictions.eq("codOfi", e.getNewValue()));
+		TiivsOficina1 tmpOfic = new TiivsOficina1();
+		
+		try {
+			lstOficina1 = ofiDAO.buscarDinamico(filtroOfic);
+		} catch (Exception exp) {
+			logger.debug("No se pudo encontrar el nombre de la oficina");
+		}
+
+		/*for (TiivsOficina1 of : lstOficina) {
+			if (lstOficina.size() == 1) {
+				tmpOfic.setCodOfi(of.getCodOfi());
+				tmpOfic.setDesOfi(of.getDesOfi());
+			}
+		}*/
+		
+		return tmpOfic;
 	}
 	
 	public void cargarCombosBandejaSeguimiento(String codigo)
@@ -391,6 +574,17 @@ public class SolicitudMB
 				
 				logger.debug("Tamanio lista de tipos de fecha: " + lstTiposFecha.size());
 			}
+			
+			//Carga lista de monedas
+			if (res.getId().getCodMult().equalsIgnoreCase("T08"))
+			{
+				Moneda tmpMoneda = new Moneda();
+				tmpMoneda.setCodMoneda(res.getId().getCodElem());
+				tmpMoneda.setDesMoneda(res.getValor1());
+				lstMoneda.add(tmpMoneda);
+				
+				logger.debug("Tamanio lista de monedas: " + lstMoneda.size());
+			}
 		}
 		
 		//Carga combo de Estudios
@@ -449,6 +643,7 @@ public class SolicitudMB
 		
 		try {
 			lstOficina = oficDAO.buscarDinamico(filtroOfi);
+			lstOficina1=oficDAO.buscarDinamico(filtroOfi);
 		} catch (Exception e) {
 			logger.debug("Error al cargar el listado de oficinas");
 		}
