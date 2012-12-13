0683be683ee671fda29bc26230ad35403320d88c
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2012-12-13 15:55:10 +0000

    [13-12-2012]: Visado de Poderes: Visor PDF para Hito 2: Registro

---
+++ trunk/VisadoPoderes/src/com/hildebrando/visado/modelo/TiivsParametros.java
@@ -0,0 +1,75 @@
+package com.hildebrando.visado.modelo;
+
+public class TiivsParametros implements java.io.Serializable {
+	private int idParam;
+	private int idEmpresa;
+	private int idSistema;
+	private String rutaLocal;
+	private String server;
+	private String loginServer;
+	private String passServer;
+	private String carpetaRemota;
+	private String urlAPP;
+	private String codUsuario;
+	
+	public int getIdParam() {
+		return idParam;
+	}
+	public void setIdParam(int idParam) {
+		this.idParam = idParam;
+	}
+	public int getIdEmpresa() {
+		return idEmpresa;
+	}
+	public void setIdEmpresa(int idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+	public int getIdSistema() {
+		return idSistema;
+	}
+	public void setIdSistema(int idSistema) {
+		this.idSistema = idSistema;
+	}
+	public String getRutaLocal() {
+		return rutaLocal;
+	}
+	public void setRutaLocal(String rutaLocal) {
+		this.rutaLocal = rutaLocal;
+	}
+	public String getServer() {
+		return server;
+	}
+	public void setServer(String server) {
+		this.server = server;
+	}
+	public String getLoginServer() {
+		return loginServer;
+	}
+	public void setLoginServer(String loginServer) {
+		this.loginServer = loginServer;
+	}
+	public String getPassServer() {
+		return passServer;
+	}
+	public void setPassServer(String passServer) {
+		this.passServer = passServer;
+	}
+	public String getCarpetaRemota() {
+		return carpetaRemota;
+	}
+	public void setCarpetaRemota(String carpetaRemota) {
+		this.carpetaRemota = carpetaRemota;
+	}
+	public String getUrlAPP() {
+		return urlAPP;
+	}
+	public void setUrlAPP(String urlAPP) {
+		this.urlAPP = urlAPP;
+	}
+	public String getCodUsuario() {
+		return codUsuario;
+	}
+	public void setCodUsuario(String codUsuario) {
+		this.codUsuario = codUsuario;
+	}
+}
