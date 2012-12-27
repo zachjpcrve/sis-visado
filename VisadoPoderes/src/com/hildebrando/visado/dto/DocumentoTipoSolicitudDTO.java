62ac6da5eeac3212cee975ab1e8e56727b3d4dca
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2012-12-27 16:23:38 +0000

    [27-12-2012]: Visado de Poderes: Visor PDF para Hito 2: Registro

--- VisadoPoderes/src/com/hildebrando/visado/dto/DocumentoTipoSolicitudDTO.java
+++ VisadoPoderes/src/com/hildebrando/visado/dto/DocumentoTipoSolicitudDTO.java
@@ -1,56 +1,72 @@
 package com.hildebrando.visado.dto;
 
 public class DocumentoTipoSolicitudDTO {
-private String item;
-private String documento;
-private String obligacion;
-private String alias;
-private boolean bobligacion;
+	private String item;
+	private String documento;
+	private String obligacion;
+	private String alias;
+	private boolean bobligacion;
+	private String rutaDoc;
 
-public DocumentoTipoSolicitudDTO(String item, String documento,String obligacion, String alias) {
-	super();
-	this.item = item;
-	this.documento = documento;
-	this.obligacion = obligacion;
-	this.alias = alias;
-	this.bobligacion = Boolean.valueOf(obligacion);
-}
-public DocumentoTipoSolicitudDTO() {
-	super();
-}
+	public DocumentoTipoSolicitudDTO(String item, String documento,
+			String obligacion, String alias) {
+		super();
+		this.item = item;
+		this.documento = documento;
+		this.obligacion = obligacion;
+		this.alias = alias;
+		this.bobligacion = Boolean.valueOf(obligacion);
+	}
 
-public String getItem() {
-	return item;
-}
-public void setItem(String item) {
-	this.item = item;
-}
-public String getDocumento() {
-	return documento;
-}
-public void setDocumento(String documento) {
-	this.documento = documento;
-}
-public String getObligacion() {
-	return obligacion;
-}
-public void setObligacion(String obligacion) {
-	this.obligacion = obligacion;
-}
+	public DocumentoTipoSolicitudDTO() {
+		super();
+	}
 
-public boolean isBobligacion() {
-	return bobligacion;
-}
-public void setBobligacion(boolean bobligacion) {
-	this.bobligacion = bobligacion;
-}
-public String getAlias() {
-	return alias;
-}
-public void setAlias(String alias) {
-	this.alias = alias;
-}
+	public String getItem() {
+		return item;
+	}
 
+	public void setItem(String item) {
+		this.item = item;
+	}
 
+	public String getDocumento() {
+		return documento;
+	}
 
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getObligacion() {
+		return obligacion;
+	}
+
+	public void setObligacion(String obligacion) {
+		this.obligacion = obligacion;
+	}
+
+	public boolean isBobligacion() {
+		return bobligacion;
+	}
+
+	public void setBobligacion(boolean bobligacion) {
+		this.bobligacion = bobligacion;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String getRutaDoc() {
+		return rutaDoc;
+	}
+
+	public void setRutaDoc(String rutaDoc) {
+		this.rutaDoc = rutaDoc;
+	}
 }
