c343be5fcd4cc0aa9041a90d4a95d6fe7a913659
eramoz@gmail.com <eramoz@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2012-12-31 17:49:12 +0000

    Grilla de documentos

--- VisadoPoderes/src/com/hildebrando/visado/modelo/TiivsTipoSolicDocumento.java
+++ VisadoPoderes/src/com/hildebrando/visado/modelo/TiivsTipoSolicDocumento.java
@@ -15,7 +15,6 @@ public class TiivsTipoSolicDocumento implements java.io.Serializable {
 	private String desDoc;
 	private char activo;
 	private char obligatorio;
-//	private Set tiivsAnexoSolicituds = new HashSet(0);
 
 	public TiivsTipoSolicDocumento() {
 	}
@@ -29,17 +28,6 @@ public class TiivsTipoSolicDocumento implements java.io.Serializable {
 		this.activo = activo;
 		this.obligatorio = obligatorio;
 	}
-
-	/*public TiivsTipoSolicDocumento(String codDoc,
-			TiivsTipoSolicitud tiivsTipoSolicitud, String desDoc, char activo,
-			char obligatorio, Set tiivsAnexoSolicituds) {
-		this.codDoc = codDoc;
-		this.tiivsTipoSolicitud = tiivsTipoSolicitud;
-		this.desDoc = desDoc;
-		this.activo = activo;
-		this.obligatorio = obligatorio;
-		this.tiivsAnexoSolicituds = tiivsAnexoSolicituds;
-	}*/
 
 	public String getCodDoc() {
 		return this.codDoc;
@@ -80,13 +68,12 @@ public class TiivsTipoSolicDocumento implements java.io.Serializable {
 	public void setObligatorio(char obligatorio) {
 		this.obligatorio = obligatorio;
 	}
-
-	/*public Set getTiivsAnexoSolicituds() {
-		return this.tiivsAnexoSolicituds;
+	
+	public boolean equals(Object object){
+		boolean ret = (object instanceof TiivsTipoSolicDocumento) && (codDoc != null) 
+	             ? codDoc.equals(((TiivsTipoSolicDocumento) object).codDoc) 
+	             : (object == this);
+		return ret;
 	}
-
-	public void setTiivsAnexoSolicituds(Set tiivsAnexoSolicituds) {
-		this.tiivsAnexoSolicituds = tiivsAnexoSolicituds;
-	}*/
 
 }
