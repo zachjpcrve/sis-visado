8694dd3ab0455e03eceda77d2341d35ccbd22631
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-01-11 22:09:41 +0000

    Creacion inicial de datos para VISPOD - 11.01.2013

---
+++ trunk/VisadoPoderes/scripts/1_SEQUENCES.sql
@@ -0,0 +1,10 @@
+/**
+ *  1_SEQUENCES.sql
+ *  Creación inicial de SECUENCIAS usadas en la aplicación Visado de Poderes.
+ *  Esquema: VISPOD 
+ *  Fecha: 02/01/2013
+ *  Versión: 1 
+ */
+
+
+COMMIT;
