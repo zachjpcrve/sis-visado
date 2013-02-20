dda6ace371abb7bbd3f68882f2288eb288132dec
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-02-20 14:49:44 +0000

    [20-02-2013]: Visado de Poderes: Reportes

--- trunk/VisadoPoderes/src/com/hildebrando/visado/mb/ReportesMB.java
+++ trunk/VisadoPoderes/src/com/hildebrando/visado/mb/ReportesMB.java
@@ -16,7 +16,6 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.event.ValueChangeEvent;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
