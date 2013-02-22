208c82a0fe1537d5c021a58ff249297db323f049
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-02-22 00:50:10 +0000

    Refactor de clase e inclusión de mensajes log. [21.02.2013]

--- VisadoPoderes/src/com/hildebrando/visado/mb/JobsMB.java
+++ VisadoPoderes/src/com/hildebrando/visado/mb/JobsMB.java
@@ -16,6 +16,7 @@ import org.apache.log4j.Logger;
 import org.hibernate.criterion.Expression;
 import org.omg.PortableInterceptor.USER_EXCEPTION;
 import com.bbva.common.listener.SpringInit.SpringInit;
+import com.bbva.common.util.ConstantesVisado;
 import com.bbva.general.entities.Feriado;
 import com.bbva.general.service.ConfigBean;
 import com.bbva.general.service.TablaGeneral;
@@ -23,6 +24,12 @@ import com.bbva.general.service.TablaGeneralServiceLocator;
 import com.bbva.persistencia.generica.dao.Busqueda;
 import com.bbva.persistencia.generica.dao.GenericDao;
 import com.bbva.persistencia.generica.dao.impl.GenericDaoImpl;
+
+/**
+ * Clase que se encarga de manejar los jobs de la aplicación: Incluye por ejemplo
+ * los feriados, etc
+ * @author  
+ * */
 
 @ManagedBean(name = "admJobs")
 @SessionScoped
@@ -37,14 +44,13 @@ public class JobsMB {
 			// 3 digitos: Distrito
 
 			Timestamp tstInicio = new Timestamp(new java.util.Date().getTime());
-			logger.debug("INICIA PROCESO CARGA FERIADOS: " + tstInicio);
+			logger.debug("==== INICIA PROCESO CARGA FERIADOS === Inicio: " + tstInicio);
 
 			Feriado[] resultado = obtenerDatosWebService().getFeriadoListado();
 
 			Timestamp tsLatencia = new Timestamp(new java.util.Date().getTime());
 			double segundosUtilizadosLat = restarFechas(tstInicio, tsLatencia);
-			logger.debug("SE DEMORO EN OBTENER LOS DATOS DEL WEB SERVICE DE FERIADOS: "
-					+ segundosUtilizadosLat + " SEGUNDOS");
+			logger.debug("Tiempo demora obtenerDatosWebService: ["+ segundosUtilizadosLat + "] segundos.");
 
 			for (Feriado feriado : resultado) {		
 				
@@ -54,111 +60,98 @@ public class JobsMB {
 				if (anio >= anioActual) {
 					
 					if (feriado.getUbigeo() != null) {
-						logger.debug("------------------FERIADOS LOCALES------------------------");
-						logger.debug("Ind. Feriado: " + feriado.getIndicador());
-						logger.debug("Fecha: "
-								+ feriado.getFecha().getTime().toGMTString());
-						logger.debug("Ubigeo: " + feriado.getUbigeo());
-						logger.debug("Descripcion: " + feriado.getDescripcion());
-						logger.debug("--------------------------------------------------");
+						if(logger.isDebugEnabled()){
+							logger.debug("------------------FERIADOS LOCALES------------------------");
+							logger.debug("Ind. Feriado: " + feriado.getIndicador());
+							logger.debug("Fecha: "+ feriado.getFecha().getTime().toGMTString());
+							logger.debug("Ubigeo: " + feriado.getUbigeo());
+							logger.debug("Descripcion: " + feriado.getDescripcion());
+							logger.debug("--------------------------------------------------");
+						}
 					} else {
-						logger.debug("------------------FERIADOS NACIONALES------------------------");
-						logger.debug("Ind. Feriado: " + feriado.getIndicador());
-						logger.debug("Fecha: "
-								+ feriado.getFecha().getTime().toGMTString());
-						logger.debug("Descripcion: " + feriado.getDescripcion());
-						logger.debug("--------------------------------------------------");
+						if(logger.isDebugEnabled()){
+							logger.debug("------------------FERIADOS NACIONALES------------------------");
+							logger.debug("Ind. Feriado: " + feriado.getIndicador());
+							logger.debug("Fecha: "+ feriado.getFecha().getTime().toGMTString());
+							logger.debug("Descripcion: " + feriado.getDescripcion());
+							logger.debug("--------------------------------------------------");
+						}
 					}
 
 					com.hildebrando.visado.modelo.TiivsFeriado ferid = new com.hildebrando.visado.modelo.TiivsFeriado();
 					
-
-					if (feriado.getIndicador().equals("L")) {
+					if (feriado.getIndicador().equals(ConstantesVisado.FERIADO.LOCAL)) {
 						ferid.setIndicador('L');
 					} else {
 						ferid.setIndicador('N');
 					}
 
-					ferid.setFecha(feriado.getFecha().getTime());
-					
+					ferid.setFecha(feriado.getFecha().getTime());					
 					ferid.setCodDist(feriado.getUbigeo());
 
 //					if (!validarSiExiste("feriado", ferid)) {
-
-						grabarFeriado(ferid);
-						
+						grabarFeriado(ferid);						
 //					}
 				}
 			}
 
 			Timestamp tstFin = new Timestamp(new java.util.Date().getTime());
-			logger.debug("TERMINA PROCESO CARGA FERIADOS: " + tstFin);
+			logger.debug("=== TERMINA PROCESO CARGA FERIADOS === Fin: " + tstFin);
 
 			double segundosUtilizados = restarFechas(tstInicio, tstFin);
-			logger.debug("PROCESO CARGA FERIADOS REALIZADO EN: "
-					+ segundosUtilizados + " SEGUNDOS");
+			logger.debug("Proceso de carga de feriados finalizado en ["+ segundosUtilizados + "] segundos");
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.debug(ConstantesVisado.MENSAJE.OCURRE_EXCEPCION+"al cargar los feriados: "+e);
 		}
 	}
 
 	@SuppressWarnings({ "unchecked" })
-	public static boolean validarSiExisteFeriado(String tabla, String campo,
-			Object valor) {
+	public static boolean validarSiExisteFeriado(String tabla, String campo,Object valor) {
 		boolean existe = false;
 
-		if (tabla.equals("feriado")) {
+		if (tabla.equals(ConstantesVisado.FERIADO.tablaferiado)) {
 			List<com.hildebrando.visado.modelo.TiivsFeriado> results = new ArrayList<com.hildebrando.visado.modelo.TiivsFeriado>();
 			GenericDaoImpl<com.hildebrando.visado.modelo.TiivsFeriado, Integer> feriadoDAO = (GenericDaoImpl<com.hildebrando.visado.modelo.TiivsFeriado, Integer>) SpringInit
 					.getApplicationContext().getBean("genericoDao");
 
-			Busqueda filtro = Busqueda
-					.forClass(com.hildebrando.visado.modelo.TiivsFeriado.class);
+			Busqueda filtro = Busqueda.forClass(com.hildebrando.visado.modelo.TiivsFeriado.class);
 			filtro.add(Expression.eq(campo, valor));
 
 			try {
 				results = feriadoDAO.buscarDinamico(filtro);
 			} catch (Exception e) {
-				e.printStackTrace();
+				logger.debug(ConstantesVisado.MENSAJE.OCURRE_EXCEPCION+"al consultar feriados: "+e);
 			}
-
 			if (results.size() > 0) {
 				existe = true;
 			}
 		}
-
 		return existe;
 	}
 
 	@SuppressWarnings({ "unchecked" })
-	public static void grabarFeriado(
-			com.hildebrando.visado.modelo.TiivsFeriado ferid) {
+	public static void grabarFeriado(com.hildebrando.visado.modelo.TiivsFeriado ferid) {
 		GenericDao<com.hildebrando.visado.modelo.TiivsFeriado, Object> feriadoDAO = (GenericDao<com.hildebrando.visado.modelo.TiivsFeriado, Object>) SpringInit
 				.getApplicationContext().getBean("genericoDao");
-
 		try {
-			if (validarSiExisteFeriado("feriado", "fecha",
-					ferid.getFecha())) {
+			if (validarSiExisteFeriado("feriado", "fecha",ferid.getFecha())) {
 				feriadoDAO.modificar(ferid);
 			} else {
 				feriadoDAO.insertar(ferid);
+				logger.debug(ConstantesVisado.MENSAJE.REGISTRO_OK+"el feriado -> Dist:"+ferid.getCodDist() + "  tipo: "+ferid.getIndicador());
 			}
 			// FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-			logger.debug("Se inserto feriado exitosamente!");
 		} catch (Exception e) {
-			e.printStackTrace();
-			FacesContext.getCurrentInstance().addMessage(
-					null,
-					new FacesMessage("No registro",
-							"No se registro el feriado desde el webservice"));
-			logger.debug("No registro el feriado!");
+			logger.debug(ConstantesVisado.MENSAJE.OCURRE_ERROR+"al grabar el feriado: "+e);
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("No registro","No se registro el feriado desde el webservice"));
 		}
 	}
 
 	public static boolean validarSiExiste(String tabla, Object obj) {
 		boolean existe = false;
-		if (tabla.equals("feriado")) {
+		if (tabla.equals(ConstantesVisado.FERIADO.tablaferiado)) {
 			/*
 			 * List<com.hildebrando.legal.modelo.Feriado> results = new
 			 * ArrayList<com.hildebrando.legal.modelo.Feriado>(); String
@@ -174,15 +167,14 @@ public class JobsMB {
 			GenericDaoImpl<com.hildebrando.visado.modelo.TiivsFeriado, Integer> feriadoDAO = (GenericDaoImpl<com.hildebrando.visado.modelo.TiivsFeriado, Integer>) SpringInit
 					.getApplicationContext().getBean("genericoDao");
 
-			Busqueda filtro = Busqueda
-					.forClass(com.hildebrando.visado.modelo.TiivsFeriado.class);
+			Busqueda filtro = Busqueda.forClass(com.hildebrando.visado.modelo.TiivsFeriado.class);
 			filtro.add(Expression.eq("fecha", feridTMP.getFecha()));			
 			filtro.add(Expression.eq("indicador", feridTMP.getIndicador()));
 
 			try {
 				results = feriadoDAO.buscarDinamico(filtro);
 			} catch (Exception e) {
-				e.printStackTrace();
+				logger.debug(ConstantesVisado.MENSAJE.OCURRE_EXCEPCION+"en la validacion feriados: "+e);
 			}
 
 			if (results.size() > 0) {
@@ -195,15 +187,12 @@ public class JobsMB {
 	public static TablaGeneral obtenerDatosWebService() {
 		TablaGeneral tbGeneralWS = null;
 		try {
-			
 			TablaGeneralServiceLocator tablaGeneralServiceLocator = (TablaGeneralServiceLocator) SpringInit
-					.getApplicationContext().getBean(
-							"tablaGeneralServiceLocator");
+					.getApplicationContext().getBean("tablaGeneralServiceLocator");
 			tbGeneralWS = tablaGeneralServiceLocator.getTablaGeneral();
 		} catch (ServiceException e) {
-			e.printStackTrace();
+			logger.debug(ConstantesVisado.MENSAJE.OCURRE_EXCEPCION+"al obtener datos del WebService: "+e);
 		}
-
 		return tbGeneralWS;
 	}
 
