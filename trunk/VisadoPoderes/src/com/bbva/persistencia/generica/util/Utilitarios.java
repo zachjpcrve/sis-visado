daa211e01a98ed295694c93e5242ffc15d3b8047
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-03-08 19:43:37 +0000

    [08-03-2013]: Visado de Poderes: Incidencias

--- trunk/VisadoPoderes/src/com/bbva/persistencia/generica/util/Utilitarios.java
+++ trunk/VisadoPoderes/src/com/bbva/persistencia/generica/util/Utilitarios.java
@@ -685,6 +685,16 @@ public class Utilitarios {
 			return fechaActualizacion + ConstantesVisado.GUION + horaActualizacion;		
 		}
 	  
+	  public static Date getDateWithoutTime(Date date) {
+		    Calendar cal = Calendar.getInstance();
+		    cal.setTime(date);
+		    cal.set(Calendar.HOUR_OF_DAY, 0);
+		    cal.set(Calendar.MINUTE, 0);
+		    cal.set(Calendar.SECOND, 0);
+		    cal.set(Calendar.MILLISECOND, 0);
+		    return cal.getTime();
+		}
+	  
 	  public static String validarCampoNull(String campo)
 		{
 			String resultado="";
