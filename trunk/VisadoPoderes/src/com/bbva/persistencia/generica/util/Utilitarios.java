716c2093ab5eded3f27a33ea5b77e0e85e9312b9
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-04-11 16:29:54 +0000

    [10-04-2013]: Visado de Poderes: Incidencias

--- trunk/VisadoPoderes/src/com/bbva/persistencia/generica/util/Utilitarios.java
+++ trunk/VisadoPoderes/src/com/bbva/persistencia/generica/util/Utilitarios.java
@@ -328,6 +328,64 @@ public class Utilitarios {
 		cell.setCellStyle(cellStyle);
 	}
 	
+	public static void crearCellRPT(Workbook wb, Row row, int column, short halign, short valign, String strContenido, boolean booBorde,
+			boolean booCabecera, boolean booFiltrosBus, Short color) 
+	{
+		CreationHelper ch = wb.getCreationHelper();
+		Cell cell = row.createCell(column);
+		cell.setCellValue(ch.createRichTextString(strContenido));
+		CellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setAlignment(halign);
+		cellStyle.setVerticalAlignment(valign);
+		
+		if (booBorde) 
+		{
+			cellStyle.setBorderBottom(HSSFCellStyle.BORDER_DOTTED);
+			cellStyle.setBottomBorderColor((short) 8);
+			cellStyle.setBorderLeft(HSSFCellStyle.BORDER_DOTTED);
+			cellStyle.setLeftBorderColor((short) 8);
+			cellStyle.setBorderRight(HSSFCellStyle.BORDER_DOTTED);
+			cellStyle.setRightBorderColor((short) 8);
+			cellStyle.setBorderTop(HSSFCellStyle.BORDER_DOTTED);
+			cellStyle.setTopBorderColor((short) 8);
+		}
+		
+		if (booCabecera) 
+		{
+			cellStyle.setBorderBottom(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setBottomBorderColor((short) 8);
+			cellStyle.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setLeftBorderColor((short) 8);
+			cellStyle.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setRightBorderColor((short) 8);
+			cellStyle.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setTopBorderColor((short) 8);
+
+			cellStyle.setFillForegroundColor(color);
+			
+			Font cellFont = wb.createFont();
+			cellFont.setColor((short) HSSFColor.WHITE.index);
+			cellStyle.setFont(cellFont);
+			
+			cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+		}
+		
+		if (booFiltrosBus) 
+		{
+			cellStyle.setBorderBottom(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setBottomBorderColor((short) 8);
+			cellStyle.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setLeftBorderColor((short) 8);
+			cellStyle.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setRightBorderColor((short) 8);
+			cellStyle.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
+			cellStyle.setTopBorderColor((short) 8);
+			//cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+		}
+		
+		cell.setCellStyle(cellStyle);
+	}
+	
 	public static void crearCell(Workbook wb, Row row, int column, short halign, short valign, String strContenido, boolean booBorde,
 			boolean booCabecera, boolean booFiltrosBus, Short color) 
 	{
