74132d6f6339ec575173a2627d50915d6455a35a
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-04-17 23:26:38 +0000

    [17-04-2013]: Visado de Poderes: Incidencias

--- trunk/VisadoPoderes/src/com/hildebrando/visado/mb/ReportesMB.java
+++ trunk/VisadoPoderes/src/com/hildebrando/visado/mb/ReportesMB.java
@@ -707,7 +707,7 @@ public class ReportesMB {
 			
 			for (String estudio : lstEstudioSelected) 
 			{
-				cadEstudio += "" + estudio + ",";
+				cadEstudio += "'" + estudio + "',";
 			}
 			cadEstudio = cadEstudio.substring(0, cadEstudio.lastIndexOf(","));
 
