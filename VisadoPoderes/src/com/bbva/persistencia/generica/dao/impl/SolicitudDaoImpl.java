912dfb18f11810104f60d1abd2fc73d14c840e63
cesar0222@gmail.com <cesar0222@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-04-17 23:26:38 +0000

    [17-04-2013]: Visado de Poderes: Incidencias

--- VisadoPoderes/src/com/bbva/persistencia/generica/dao/impl/SolicitudDaoImpl.java
+++ VisadoPoderes/src/com/bbva/persistencia/generica/dao/impl/SolicitudDaoImpl.java
@@ -376,7 +376,7 @@ public abstract class SolicitudDaoImpl<K, T extends Serializable> extends
 				}
 				else
 				{*/
-					sWhere += " and ts.cod_tip_solic in ('" + cadTipoServ + "')";
+					sWhere += " and ts.cod_tip_solic in (" + cadTipoServ + ")";
 				//}
 			}		
 			
@@ -471,7 +471,7 @@ public abstract class SolicitudDaoImpl<K, T extends Serializable> extends
 				}
 				else
 				{*/
-					sWhere += " and so.cod_estudio in ('" + cadEstudio + "')";
+					sWhere += " and so.cod_estudio in (" + cadEstudio + ")";
 				//}
 			}
 			
@@ -698,7 +698,7 @@ public abstract class SolicitudDaoImpl<K, T extends Serializable> extends
 			if (cadEstudio.compareTo("")!=0)
 			{
 				logger.info("Buscando por estudio: " + cadEstudio );
-				sWhere += " and so.cod_estudio in ('" + cadEstudio + "') ";
+				sWhere += " and so.cod_estudio in (" + cadEstudio + ") ";
 			}
 			
 			if (anio!=0)
