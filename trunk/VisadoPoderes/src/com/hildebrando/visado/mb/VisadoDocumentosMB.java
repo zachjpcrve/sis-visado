2ca1ce8d4205244a606195094d2a24864169656f
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-08-07 15:01:04 +0000

    [05.08.2013] Inclusion de mensajes log

--- trunk/VisadoPoderes/src/com/hildebrando/visado/mb/VisadoDocumentosMB.java
+++ trunk/VisadoPoderes/src/com/hildebrando/visado/mb/VisadoDocumentosMB.java
@@ -1,48 +1,26 @@
 package com.hildebrando.visado.mb;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.StringBufferInputStream;
-import java.net.MalformedURLException;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-import javax.servlet.ServletContext; 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
-import org.w3c.dom.Document;
-import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import com.bbva.common.listener.SpringInit.SpringInit;
 import com.bbva.common.util.ConstantesVisado;
 import com.bbva.persistencia.generica.dao.Busqueda;
 import com.bbva.persistencia.generica.dao.GenericDao;
-import com.bbva.persistencia.generica.util.Utilitarios;
-import com.grupobbva.bc.per.tele.ldap.serializable.IILDPeUsuario;
-import com.hildebrando.visado.modelo.TiivsMultitabla;
-import com.hildebrando.visado.modelo.TiivsMultitablaId;
 import com.hildebrando.visado.modelo.TiivsParametros;
 import com.hildebrando.visado.modelo.TiivsTipoSolicDocumento;
 
 /**
- * 
+ * Clase encargada de inicializar los parámetros de BD y  ademas se 
+ * encarga de invocar al servlet para la carga de archivos
+ * al directorio compartido en el FileServer.
  * @author 
  * @version 1.0
  */
@@ -94,8 +72,8 @@ public class VisadoDocumentosMB {
 			
 			logger.info("[PARAMETROS_BD]-pathCliente:" + pathCliente);
 			logger.info("[PARAMETROS_BD]-host:" + host);
-			logger.info("[PARAMETROS_BD]-user:" + user);
-			logger.info("[PARAMETROS_BD]-pass:" + pass);
+			logger.debug("[PARAMETROS_BD]-user:" + user);
+			logger.debug("[PARAMETROS_BD]-pass:" + pass);
 			logger.info("[PARAMETROS_BD]-directory:" + directory);
 			
 		}		
@@ -119,7 +97,7 @@ public class VisadoDocumentosMB {
 		}catch(Exception e){
 			logger.error(ConstantesVisado.MENSAJE.OCURRE_ERROR + " al obtener la ruta de carga de archivos",e);
 		}
-		logger.debug("urlCarga: "+url);
+		logger.debug("[MODIFICADO ok ]-urlCarga: "+url);
 		return url;
 	}
 
