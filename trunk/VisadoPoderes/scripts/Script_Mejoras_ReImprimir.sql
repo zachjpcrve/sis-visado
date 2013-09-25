76b2e3b07d6fc826ad23717e44afa3ab376326da
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-09-25 01:49:03 +0000

    [24.09.2013] Generacion de Script para REIMPRESION DE CARTA

--- trunk/VisadoPoderes/scripts/Script_Mejoras_ReImprimir.sql
+++ trunk/VisadoPoderes/scripts/Script_Mejoras_ReImprimir.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "VISPOD"."TIIVS_SOLICITUD" ADD (FLAG_REIMPRIMIR VARCHAR2(1) DEFAULT NULL );
 
-update "VISPOD"."tiivs_solicitud" set flag_reimprimir =0 ;
+update "VISPOD"."TIIVS_SOLICITUD" set flag_reimprimir =0 ;
