d33fa78d11faa275766e7a4aab4e944fe50593c5
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-09-30 19:33:06 +0000

    Quinto entregable: actualización envío de ficheros por FTP

--- trunk/UploadFilesApplet/src/com/hildebrando/visado/ftp/ClienteFTP.java
+++ trunk/UploadFilesApplet/src/com/hildebrando/visado/ftp/ClienteFTP.java
@@ -4,24 +4,28 @@
  */
 package com.hildebrando.visado.ftp;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
+import com.hildebrando.visado.util.Constantes;
+
 /**
- * Clase que se encarga de abrir una conexión FTP para el UpLoad de
- * archivos. También contempla renombrar archivos y seteo de directorio
- * de trabajo para los documentos. 
+ *
  * @author Hildebrando
  */
 public class ClienteFTP {
 
-    private String username, password, host;
+    private String username, password, host, directorio;
     private boolean login;
     FTPClient ftpCliente;
 
-    public ClienteFTP(String host, String username, String password) {
+    /*public ClienteFTP(String host, String username, String password) {
         this.username = username;
         this.password = password;
         this.host = host;
@@ -39,19 +43,31 @@ public class ClienteFTP {
             System.out.println("Error al conectarse al FTP debido a: " + e.getMessage());
             e.printStackTrace();
         }
+    }*/
+    
+    public ClienteFTP(String host, String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.host = host;
+        ftpCliente = new FTPClient();        
+    }
+    
+    public ClienteFTP(String host, String username, String password, String directorio) {
+        this.username = username;
+        this.password = password;
+        this.host = host;
+        this.directorio = directorio;
+        ftpCliente = new FTPClient();        
     }
 
     public void setDirectorio(String directorio) throws IOException {
         ftpCliente.changeWorkingDirectory(directorio);
     }
 
-    
-
     public void upLoadFiles(String file, String ruta) {
-    	System.out.println("== upLoadFiles:Inicio ==");
         FileInputStream fis = null;
+
         try {
-        	System.out.println("\tArchivo:"+file+"  Ruta:"+ruta);
             fis = new FileInputStream(file);
             ftpCliente.setFileType(FTP.BINARY_FILE_TYPE);
             ftpCliente.setFileTransferMode(FTP.BINARY_FILE_TYPE);
@@ -59,12 +75,8 @@ public class ClienteFTP {
             ftpCliente.storeFile(file, fis);
 
         } catch (IOException e) {
-        	System.out.println("Ha ocurrido una IOException en upLoadFiles:"+e);
             e.printStackTrace();
-        } catch(Exception exc){
-        	System.out.println("Ha ocurrido una Exception en upLoadFiles:"+exc);
-        } 
-        finally {
+        } finally {
             try {
                 if (fis != null) {
                     fis.close();
@@ -72,54 +84,97 @@ public class ClienteFTP {
                 // ftpCliente.disconnect();
             } catch (IOException e) {
                 e.printStackTrace();
-                System.out.println("Ha ocurrido una IOException en upLoadFiles:"+e);
             }
         }
     }
     
+    //Metodo que se agrega para subir archivos por FTP
+    public String uploadFiles(List<File> listaFile){
+    	System.out.println("********uploadFiles:Inicio*************"); 
+    	String archivosSubidos = "";
+    	Integer contador = 0;
+    	String nombreTemporal = null;
+    	try {
+            ftpCliente.connect(this.host);
+            this.login = ftpCliente.login(this.username, this.password);
+            if (this.login) {
+                System.out.println("Login success...");
+                ftpCliente.changeWorkingDirectory(directorio);
+                for(File file:listaFile){
+                	contador++;
+                	FileInputStream fis = new FileInputStream(file);
+                	ftpCliente.setFileType(org.apache.commons.net.ftp.FTP.BINARY_FILE_TYPE);
+                	nombreTemporal= createNameFile(file);
+                	boolean res = ftpCliente.storeFile(nombreTemporal, fis );
+                	System.out.println("Resultado de subir el archivo es " + res);
+                	fis.close();
+                	
+                	if(contador<listaFile.size()){
+                		archivosSubidos = archivosSubidos + nombreTemporal + Constantes.SEPARADOR;
+                	}else{
+                		archivosSubidos = archivosSubidos + nombreTemporal;
+                	}
+                }
+            } else {
+                System.out.println("Failure success...");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al conectarse al FTP debido a: " + e.getMessage());
+            e.printStackTrace();
+        } finally{
+        	try {
+        		if(ftpCliente!=null){
+        			ftpCliente.logout();
+    				ftpCliente.disconnect();	
+        		}
+			} catch (IOException e) {
+				System.out.println("+++ Fallo al desconectar FTP");
+			}
+        }
+    	System.out.println("********uploadFiles:Fin*************"); 
+    	return archivosSubidos;
+    }
+    
+    private String createNameFile(File file){
+    	if(file!=null){
+    		long fecha = new Date().getTime();
+    		String nombreArchivo = file.getName().substring(0, file.getName().indexOf("."));
+    		String extension = file.getName().substring(file.getName().lastIndexOf("."));
+    		String nombre =  nombreArchivo + "_" + fecha + extension;
+    		return nombre;
+    	}
+    	return null;
+    }
+    
     public void renombrarArchivo(String nuevoNombre, String antiguoNombre) {
-    	System.out.println("== renombrarArchivo:Inicio ==");
         try {
-        	System.out.println("[Nombre]-Antiguo:"+antiguoNombre + "   Nuevo:"+nuevoNombre);
             ftpCliente.rename(antiguoNombre, nuevoNombre);
             ftpCliente.disconnect();
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Ha ocurrido una IOException en renombrarArchivo:"+e);
-        } catch(Exception exc){
-        	exc.printStackTrace();
-        	System.out.println("Ha ocurrido una Exception en renombrarArchivo:"+exc);
         }
-        System.out.println("== renombrarArchivo:Fin ==");
     }
     
     public void upLoadOneFiles(String file, String ruta) {
         FileInputStream fis = null;
-        System.out.println("[UploadFT]-Ruta: " + ruta);
+        System.out.println("Ruta: " + ruta);
         try {
             fis = new FileInputStream(ruta);
             ftpCliente.setFileType(FTP.BINARY_FILE_TYPE);
             ftpCliente.setBufferSize(4096);
             ftpCliente.setFileTransferMode(FTP.BINARY_FILE_TYPE);
             ftpCliente.storeFile(file, fis);
-            System.out.println("[UploadFT]-file:"+file);
 
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Ha ocurrido un IOException-upLoadOneFiles:"+e);
-        }catch(Exception ex){
-        	ex.printStackTrace();
-        	System.out.println("Ha ocurrido una Exception-upLoadOneFiles:"+ex);
-        } 
-        finally {
+        } finally {
             try {
                 if (fis != null) {
                     fis.close();
                 }
 //               ftpCliente.disconnect();
-            } catch (IOException e2) {
-                e2.printStackTrace();
-                System.out.println("Ha ocurrido un IOException e2-upLoadOneFiles:"+e2);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
