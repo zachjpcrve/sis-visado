af14175c3910fd38cd4635777080bd3ad4315b8b
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-10-04 23:51:05 +0000

    [04.10.2013] Inclusion de logs y Refactor de clases

--- VisadoPoderes/src/com/hildebrando/visado/quartz/jobs/QuartzJob_Files.java
+++ VisadoPoderes/src/com/hildebrando/visado/quartz/jobs/QuartzJob_Files.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
@@ -15,7 +13,6 @@ import org.quartz.JobExecutionException;
 
 import com.bbva.common.util.ConstantesVisado;
 import com.bbva.persistencia.generica.util.Utilitarios;
-import com.hildebrando.visado.mb.JobsMB;
 
 public class QuartzJob_Files implements Job  {
 	
@@ -26,97 +23,79 @@ public class QuartzJob_Files implements Job  {
 		limpiarDirectorioFiles();	
 	}	
 
-	/*
+	/**
 	 * Realiza la limpieza de los archivos temporales que 
 	 * se almacena en la carpeta "files" de la ruta del File Server
-	 * 
 	 * */
 	public void limpiarDirectorioFiles(){
-		
-				
+		logger.info("==== limpiarDirectorioFiles() ==== ");
 		String path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-		
-		logger.info("path:" + path);
+		logger.info("[LimpiarFiles]-Path:" + path);
 		
 		path = path.substring(0,path.lastIndexOf("WEB-INF")) + ConstantesVisado.FILES + File.separator;
 		path = path.replace("/", File.separator);
+		logger.info("[LimpiarFiles]-Path con FileSystem: " + path);
 		
-		logger.info("path with FS:" + path);
-				
 		File directory = null;
-		
 		if(new File(path).exists()==false){
-			path = path.substring(1,path.length());	//Windows path
-			logger.info("modify path:" + path);
-		} 
-		
-		directory = new File(path);
-				
-		logger.info("directory exists?:" + directory.exists());
-		
-		try {
-			logger.info("Se procederá a la limpieza de archivos de la carpeta:" + directory.getAbsolutePath());
-			limpiarDirectorio(directory);
-		} catch (Exception e){
-			logger.info(ConstantesVisado.MENSAJE.OCURRE_ERROR + ":" + e.getMessage());
+			path = path.substring(1,path.length());	//Path Windows
+			logger.info("[LimpiarFiles]-Path modificado: " + path);
 		}
-		
-		
-		directory = new File(
-				Utilitarios.getPropiedad(ConstantesVisado.KEY_PATH_FILE_SERVER)
-						+ File.separator + ConstantesVisado.FILES);
-		
+		//Limpiar '/files/' - temp
+		directory = new File(path);
+		logger.info("[LimpiarFiles]-Existe directorio:" + directory.exists());
 		try {
-			logger.info("Se procederá a la limpieza de archivos de la carpeta:" + directory.getAbsolutePath());
+			logger.info("[LimpiarFiles]-Proc[1]:Se procedera con la limpieza de archivos de la carpeta:" + directory.getAbsolutePath());
 			limpiarDirectorio(directory);
 		} catch (Exception e){
-			logger.info(ConstantesVisado.MENSAJE.OCURRE_ERROR + ":" + e.getMessage());
-		}		
-
+			logger.error(ConstantesVisado.MENSAJE.OCURRE_ERROR+":",e);
+		}
+		//Limpiar '/files/'
+		directory = new File(Utilitarios.getPropiedad(ConstantesVisado.KEY_PATH_FILE_SERVER)
+			+ File.separator + ConstantesVisado.FILES);
+		try {
+			logger.info("[LimpiarFiles]-Proc[2]:Se procedera con la limpieza de archivos de la carpeta:" + directory.getAbsolutePath());
+			limpiarDirectorio(directory);
+		} catch (Exception e){
+			logger.info(ConstantesVisado.MENSAJE.OCURRE_ERROR + ":",e);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
-	private void limpiarDirectorio(File directory) {
-		
+	private void limpiarDirectorio(File directory) {	
+		logger.debug("== limpiarDirectorio():INICIO ==");
 		int diferenciaEnDias = 1;
 		Date fechaActual = Calendar.getInstance().getTime();
 		long tiempoActual = fechaActual.getTime();
 		long unDia = diferenciaEnDias * 24 * 60 * 60 * 1000;
 		Date fechaAyer = new Date(tiempoActual - unDia);
-		logger.debug("Inciando tarea de limpieza de archivos:");
-		logger.debug("Fecha corte :" + fechaAyer);
-
+		logger.debug("Iniciando tarea de limpieza de archivos:");
+		logger.debug("[limpiarDirectorio]-Fecha corte :" + fechaAyer);
+		//Si el directorio existe
 		if (directory.exists()) {
-
 			List<File> files = null;
 			String ext[] = { "pdf", "PDF" };
 			files = (List<File>) FileUtils.listFiles(directory, ext, true);
-
-			logger.debug("Numero de archivos PDF en el directorio:"
+			logger.debug("[limpiarDirectorio]-Nro archivos(PDF) a depurar:"
 					+ FileUtils.listFiles(directory, ext, false).size());
-
 			int num1 = 0, num2 = 0;
 			for (File f : files) {
-
 				if (FileUtils.isFileOlder(f, fechaAyer)) {
-					logger.info("Eliminar: " + f.getName());
+					logger.info("[limpiarDirectorio]-Archivo-Depurar: " + f.getName());
 					f.delete();
 					num1++;
 				} else {
-					logger.info("Mantener: " + f.getName());
+					logger.info("[limpiarDirectorio]-Archivo-Mantener: " + f.getName());
 					num2++;
 				}
 			}
-
-			logger.debug("Número de archivos eliminados: " + num1);
-			logger.debug("Número de archivos mantenidos: " + num2);
-
+			logger.debug("[limpiarDirectorio]-Nro archivos (depurados): " + num1);
+			logger.debug("[limpiarDirectorio]-Nro archivos (mantenidos): " + num2);
 		} else {
-			logger.info("Ubicación de carpeta no existe:");
+			logger.info("El directorio de depuracion no existe.");
 		}
-
+		logger.debug("== limpiarDirectorio():FIN ==");
 	}
-	
 	
 }
  
