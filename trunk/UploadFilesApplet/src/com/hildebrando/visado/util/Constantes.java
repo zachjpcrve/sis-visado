8de357694a70f5b4fb6c3d1e13294dc0f86c9f29
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2013-10-04 23:25:04 +0000

    [04.10.2013] Implementacion para escribir logs en un archivo / refactor de clases.

--- trunk/UploadFilesApplet/src/com/hildebrando/visado/util/Constantes.java
+++ trunk/UploadFilesApplet/src/com/hildebrando/visado/util/Constantes.java
@@ -14,5 +14,19 @@ public class Constantes {
     public static final String DOCUMENTOS_LEER = "documentos_leer";
     public static final String URL_JAVA_SCRIPT = "javascript:";
 	public static final String URL_SERVER = "url_server";
+	
+	//04-10 Constantes para archivo log
+	public static final String FORMATO_FECHA = "dd_MM_yyyy";
+	public static final String FORMATO_HORA = "HH:mm:ss";
+	public static final String FORMATO_RUTA_SISTEMA = "D:\\VisadoPoderes\\";
+	public static final String FORMATO_RUTA_LOG = "Log\\";
+	public static final String FORMATO_ARCHIVO_GENERAR_LOG = "log"; //txt
+	public static final String FORMATO_NOMBRE_ARCHIVO_LOG = "log_Visado";
+	
+	public static final String MSJ_ARCHIVO_EXISTS = "El archivo ya existe, se escribiran los mensajes";
+	public static final String MSJ_ARCHIVO_NO_EXISTS = "El archivo no existe, se procedera a crearlo";
+	public static final String MSJ_INICIAL_APPLET = "LOG DE EVENTOS - VISADO DE PODERES";
+	public static final String MSJ_OCURRE_EXCEPCION = "Ha ocurrido una excepcion ";
+	public static final String MSJ_OCURRE_ERROR = "Ha ocurrido un error ";
     
 }
