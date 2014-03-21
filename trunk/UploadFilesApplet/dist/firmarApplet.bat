6d0aa860907817b07775f47cd5e3ace1b190e3e4
dieg209@gmail.com <dieg209@gmail.com@9b0467ec-5311-f875-a0a9-8d9d1b00eae4>
2014-03-21 18:34:04 +0000

    [21-03-2014]Mejoras applet Visado

--- trunk/UploadFilesApplet/dist/firmarApplet.bat
+++ trunk/UploadFilesApplet/dist/firmarApplet.bat
@@ -1,23 +1,43 @@
+REM ---- Aqui inicializamos algunas variables para la configuracion.
 @echo off
-REM --- Para firmar todos los JAR's. Si alguna vez hubiere mas librerias
-REM --- agregarlas aqui, teniendo en cuenta la sintaxis adecuada.
-
 REM : Nombre de la llave
-set SIGN_NAME=bbva-applet
-REM : Clave keystore 
+set SIGN_NAME=keyfin
+REM : Clave keystore
 set PASS=javajava
+REM : Algunos valores para la descripcion del certificado
+set CN=BBVA
+set OU=Gestion Demanda
+set O=BBVA Continental
+set L=Lima
+set S=Lima
+set C=PE
+REM : Periodo de validez certificado (Ejm: 9999 dias)
+set VALIDEZ=9999
+REM : Nombre de certificado de aplicacion
+set NOMB_CERT=certifVisPod.crt
+REM : Nombre del JAR de la aplicacion Visado
+set NOMB_JAR=uploadFilesApplet.jar
 
-rem set JAVA_PATH=C:\Program Files\Java\jdk1.6.0_32\bin
 
-rem set NOMB_JAR=D:\HDO\NetBeansProjects\firmadoapplet\ConfiguraMA-\uploadFilesApplet.jar
-rem set NOMB_JAR=D:\HDO\NetBeansProjects\firmadoapplet\ConfiguraMA-\UploadFileApplet.jar
+echo ====== INICIAR CMD VISADO PODERES =================
+d:
+echo ====== Setear el PATH JAVA
+path = C:\Archivos de programa\Java\jdk1.6.0_22\bin
 
-rem set NOMBRE_LIB=D:\HDO\NetBeansProjects\firmadoapplet\ConfiguraMA-\lib\commons-net-3.2.jar
+pause
 
-echo ========================================
-echo = FIRMADO DE JARS - VMA
-echo ========================================
-jarsigner -storepass %PASS% -keypass %PASS% -keystore %PASS% uploadFilesApplet.jar %SIGN_NAME%
-rem "%JAVA_PATH%\jarsigner" -storepass %PASS% -keypass %PASS% -keystore %PASS% "%NOMBRE_LIB%" %SIGN_NAME%
-echo ============ Proceso de firmado concluido ==========
+
+echo ====== CONFIGURACION APPLET - VISADO PODERES =================
+echo = ----------- GENERACION DE LLAVE --------------
+keytool -genkey -alias %SIGN_NAME% -validity %VALIDEZ% -dname "CN=%CN%, OU=%OU%, O=%O%, L=%L%, S=%S%, C=%C%" -v -keystore %PASS% -storepass %PASS% -keypass %PASS%
+pause
+echo = ------------ EXPORTAR CERTIFICADO ------------
+keytool -export -alias %SIGN_NAME% -file %NOMB_CERT% -keystore %PASS% -storepass %PASS%
+pause
+echo = ------------ FIRMADO DEL ARCHIVO JAR -----------------
+jarsigner -keystore %PASS% -storepass %PASS% -keypass %PASS% %NOMB_JAR% %SIGN_NAME% 
+pause
+echo = ----------- VERIFICACION DE FIRMA ------------
+jarsigner -verify -verbose -certs %NOMB_JAR%
+echo = -------- El proceso de firmado ha concluido ----
 pause
